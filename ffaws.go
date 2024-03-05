26c3e40b7bc6ebed5b339ea540869b9d8bf37f80
Dev <gaston.siffert@gmail.com>
2024-03-06 00:06:15 +0100

    Update the godoc

---
+++ ffaws.go
@@ -0,0 +1,4 @@
+// Package ffaws provides multiple feature-flags sources for AWS services:
+//   - github.com/gsiffert/ffaws/ffsecretmanager: AWS SecretManager
+//   - github.com/gsiffert/ffaws/ffappconfig: AWS AppConfig
+package ffaws
